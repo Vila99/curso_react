f96b8c6b7985e3815e6bb53bfc8873ebf330c751
Guillem Vila <guillemvilapamies@gmail.com>
2024-08-27 00:35:44 +0200

    Carousel componente solucionado

--- eventosPueblo/eventowns/app/component/CarouselClient.js
+++ eventosPueblo/eventowns/app/component/CarouselClient.js
@@ -1,35 +1,81 @@
+"use client"; // Indica que este componente es un Client Component
 
-'use client'
-import React, { Component } from 'react'
-import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../globals.css'
 
-export default function CarouselClient(){
-    return(
-        <Carousel>
-        {/* PRIMER SLIDE */}
-            <Carousel.Item>
-                <img
-                        className="d-block w-100"
-                        src="eventosPueblo\eventowns\public\img\carousel2.jpeg"
-                        alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>E-SCooter</h3>
-                    <p>Desplázate de una forma diferente</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        {/* SEGUNDO SLIDE */}
-            <Carousel.Item>
-            <img
-                className="d-block w-100"
-                src="eventosPueblo\eventowns\public\img\carousel1.jpg"
-                alt="First slide"
-              />
-            <Carousel.Caption>
-                <h3>Scooter segunda mano</h3>
-                <p>Conectamos directamente al vendedor y comprado</p>
-            </Carousel.Caption>
-            </Carousel.Item>
-      </Carousel>
-    )
-}
+const Carousel = () => {
+  return (
+    <div id="myCarousel" className="carousel slide mb-6" data-bs-ride="carousel">
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="/img/carousel2.jpeg" alt="festival" style={{ width: '100%', height: 'auto' }} />
+          <div className="container">
+            <div className="carousel-caption">
+              <h1 className="titulo2 d-flex justify-content-center">What is going on in your town?</h1>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="/img/carousel1.jpg" alt="festival" style={{ width: '100%', height: 'auto' }} />
+          <div className="container">
+            <div className="carousel-caption">
+              <h1 className="titulo2">Look what is near you!</h1>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="/img/carousel3.jpg" alt="festival" style={{ width: '100%', height: 'auto' }} />
+          <div className="container">
+            <div className="carousel-caption">
+              <h1 className="titulo2">Find new events every week!</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#myCarousel"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#myCarousel"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
